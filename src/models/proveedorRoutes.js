290c01c5f09e37e4f7256ae158572686249fe75a
Daniel Peñafiel <luis.chavez.penafiel@uagraria.edu.ec>
2024-12-18 21:14:24 -0500

    Update proveedorRoutes.js

--- src/models/proveedorRoutes.js
+++ src/models/proveedorRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { agregarProveedor, eliminarProveedor, obtenerProveedores } = require('../controllers/proveedorController');
 
 // Rutas de proveedores
-router.post('/api/add', agregarProveedor); // Agregar proveedor
-router.delete('/api/delete/:ruc', eliminarProveedor); // Eliminar proveedor por RUC
-router.get('/api/proveedores', obtenerProveedores); // Obtener todos los proveedores
+router.post('/add', agregarProveedor); // Agregar proveedor
+router.delete('/:ruc', eliminarProveedor); // Eliminar proveedor por RUC
+router.get('/proveedores', obtenerProveedores); // Obtener todos los proveedores
 
 module.exports = router;
